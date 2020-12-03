209bb2609d6c5d5b3c0f99a1abcea7b82e8dc050
Gustavo Pacola Gonçalves <Gusgo99@users.noreply.github.com>
2020-12-03 19:44:27 -0300

    Spaces changed to tabs on the solution of day 2
    Thanks VScode

--- src/day2_password_philosophy.cpp
+++ src/day2_password_philosophy.cpp
@@ -12,13 +12,13 @@ size_t solve_part_two(const std::vector<entry> &_entries);
 int main() {
 	std::vector<entry> _entries = read_entries_from_file();
 
-    size_t _solutionPart1 = solve_part_one(_entries);
+	size_t _solutionPart1 = solve_part_one(_entries);
 
-    std::cout << "Valid passwords for shopkeeper's old job: " << _solutionPart1 << std::endl;
+	std::cout << "Valid passwords for shopkeeper's old job: " << _solutionPart1 << std::endl;
 
-    size_t _solutionPart2 = solve_part_two(_entries);
+	size_t _solutionPart2 = solve_part_two(_entries);
 
-    std::cout << "Actual valid passwords: " << _solutionPart2 << std::endl;
+	std::cout << "Actual valid passwords: " << _solutionPart2 << std::endl;
 
 	return 0;
 }
@@ -51,26 +51,26 @@ std::vector<entry> read_entries_from_file() {
 }
 
 size_t solve_part_one(const std::vector<entry> &_entries) {
-    size_t _validCount = 0;
-    for(const auto &[_min, _max, _c, _str]: _entries) {
-        size_t _found = std::count(_str.begin(), _str.end(), _c);
-        if((_found >= _min) && (_found <= _max)) {
-            _validCount++;
+	size_t _validCount = 0;
+	for(const auto &[_min, _max, _c, _str]: _entries) {
+		size_t _found = std::count(_str.begin(), _str.end(), _c);
+		if((_found >= _min) && (_found <= _max)) {
+			_validCount++;
 
-        }
-    }
+		}
+	}
 
-    return _validCount;
+	return _validCount;
 }
 
 size_t solve_part_two(const std::vector<entry> &_entries) {
-    size_t _validCount = 0;
-    for(const auto &[_position1, _position2, _c, _str]: _entries) {
-        if((_str[_position1 - 1] == _c) + (_str[_position2 - 1] == _c) == 1) {
-            _validCount++;
+	size_t _validCount = 0;
+	for(const auto &[_position1, _position2, _c, _str]: _entries) {
+		if((_str[_position1 - 1] == _c) + (_str[_position2 - 1] == _c) == 1) {
+			_validCount++;
 
-        }
-    }
+		}
+	}
 
-    return _validCount;
+	return _validCount;
 }
