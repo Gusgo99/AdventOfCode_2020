667ecc1464931e888db489a90abb130b2bd082b7
Gustavo Pacola Gonçalves <gustavo@crazy4options.com>
2020-12-10 22:41:20 -0300

    Improved performance of day 4 solution

--- src/day4_passport_processing.cpp
+++ src/day4_passport_processing.cpp
@@ -36,7 +36,7 @@ std::vector<entry> read_entries_from_file() {
 		while(!_input.eof()) {
 			std::getline(_input, _passportLine);
 			if(_passportLine == "") {
-				_entries.push_back(_passportInfo);
+				_entries.push_back(_passportInfo + " ");
 				_passportInfo = "";
 
 			}
@@ -67,23 +67,21 @@ size_t solve_part_one(const std::vector<entry> &_entries) {
 }
 
 size_t solve_part_two(const std::vector<entry> &_entries) {
-	const std::array<std::string, 7> REGEXES = {
-		"byr:(19[2-9][0-9]|200[0-2])",
-		"iyr:20(1[0-9]|20)",
-		"eyr:20(2[0-9]|30)",
-		"hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in)",
-		"hcl:#[0-9a-f]{6}",
-		"ecl:(amb|blu|brn|gry|grn|hzl|oth)",
-		"pid:[0-9]{9}"
+	const std::array<std::regex, 7> REGEXES = {
+		std::regex(" byr:(19[2-9][0-9]|200[0-2]) "),
+		std::regex(" iyr:20(1[0-9]|20) "),
+		std::regex(" eyr:20(2[0-9]|30) "),
+		std::regex(" hgt:(1([5-8][0-9]|9[0-3])cm|(59|6[0-9]|7[0-6])in) "),
+		std::regex(" hcl:#[0-9a-f]{6} "),
+		std::regex(" ecl:(amb|blu|brn|gry|grn|hzl|oth) "),
+		std::regex(" pid:[0-9]{9} ")
 	};
 	size_t _validCount = 0;
-	std::regex _match;
 
 	for(auto &i: _entries) {
 		bool _valid = true;
-		for(auto j: REGEXES) {
-			_match = std::regex(" " + j + " ");
-			_valid &= std::regex_search(" " + i + " ", _match);
+		for(auto &j: REGEXES) {
+			_valid &= std::regex_search(i, j);
 		}
 		if(_valid) _validCount++;
 	}
